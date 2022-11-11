b0d7500209c6e1dcf7bbcd9ae8e4fae75aab1706
MuxoBear <semargla2@mail.ru>
2022-11-11 12:27:42 +0700

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,23 @@ public class Main {
     public static void main(String[] args) {
         // Задание 1
         System.out.println("Задание 1");
+        int age = 66;
+        if (age >=18){
+            System.out.println("Поздравляю вас с совершеннолетием");
+        }
+        if (age <18){
+            System.out.println("Возраст совершеннолетие еще не наступил, нужно подождать");
+        }
+        //задание 2
+        System.out.println("Задание 2");
+        if (age >= 7 && age <18){
+            System.out.println("Ребенок ходит в школу");
+        }
+        if (age >18 && age <=24){
+            System.out.println("Человек ходит в университет");
+        }
+        if (age > 24){
+            System.out.println("Человек ходит на работу");
+        }
     }
 }
