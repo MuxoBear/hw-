3c0912d6aa12536d0c29a52e4a3fd9c1feb6f274
MuxoBear <semargla2@mail.ru>
2022-11-11 13:22:01 +0700

    Задание 5

--- src/Main.java
+++ src/Main.java
@@ -44,6 +44,30 @@ public class Main {
 
         // Задание 4
         System.out.println("Задание 4");
+        int age1 = 8;
+        if (age1 >=2 && age1 < 6){
+            System.out.println("Если возраст человека " + age1+ " то ему нужно ходить в садик");
+        } else if (age1 >6 && age1 <=18){
+            System.out.println("Если возраст человека " + age1+ " то ему нужно ходить в школу");
+        } else if (age1 >18 && age1 <= 24){
+            System.out.println("Если возраст человека " + age1+ " то ему нужно ходить в универ");
+        } else if (age1 >24){
+            System.out.println("Если возраст человека " + age1+ " то ему нужно ходить на работу");
+        }
+
+        // задание 5
+        System.out.println("Задание 5");
+        if (age1 <5){
+            System.out.println("Ребенок не может кататься на отракционе");
+        } else if (age1 >5 && age1 <14){
+            System.out.println("ребенок может кактаться в присутствии взрослого");
+        } else if (age1 >14){
+            System.out.println("Ребенок может кататься на отракционе сам");
+        }
+
+        // Задание 6
+        System.out.println("Задание 6");
+
 
 
 
